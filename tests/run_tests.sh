5454455fc3e6d47af831a25d1a5f09173c0ee3e7
kmmbvnr <kmmbvnr@gmail.com>
2010-06-02 08:38:13 +0700

    Merge test scripts

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -24,5 +24,33 @@ pylint --rcfile=$ROOT_DIR/.pylintrc django_nose > pylint.out || echo 'PyLint don
 tail -n5 pylint.out
 
 # existing tests
-cd $ROOT_DIR/tests
-./runtests.sh
+export PYTHONPATH=$ROOT_DIR:$ROOT_DIR/tests
+
+django_test() {
+    TEST="django-admin.py test --settings=testapp.$1"
+    $TEST 2>&1 | grep 'Ran 1 test' > /dev/null
+    if [ $? -gt 0 ]
+    then
+        echo FAIL: $2
+        $TEST
+        exit 1;
+    else
+        echo PASS: $2
+    fi
+
+    # Check that we're hijacking the help correctly.
+    $TEST --help 2>&1 | grep 'NOSE_DETAILED_ERRORS' > /dev/null
+    if [ $? -gt 0 ]
+    then
+        echo FAIL: $2 '(--help)'
+        exit 1;
+    else
+        echo PASS: $2 '(--help)'
+    fi
+}
+
+set +e
+django_test 'settings' 'normal settings'
+django_test 'settings_with_south' 'with south in installed apps'
+django_test 'settings_old_style' 'django_nose.run_tests format'
+
